7511745b77136de4dc3f098274ed1321d747e1c9
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-05-16 13:08:59 -0300

    começo

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -2,3 +2,22 @@ print(1+10)
 print(1.5 + 4.3)
 print(True)
 print("sla")
+
+#dir() retorna uma lista de atrubutos no escopo local atual
+#di(100) 100 é inteiro entao vai retornar todos os metodos da classe int
+
+#variavel
+idade = 2
+print(f'essa criança tem {idade} anos')
+
+#python NAO TEM CONSTANTE
+
+#o jeito de declarar uma constante é usando o nome do tipo em maiusculo
+ABS_PATH= '/Users/ennya.campos/Documents/GitHub/Python'
+DEBUG = True
+STATES = [
+    'SP',
+    'RJ',
+    'MG,'
+]
+AMOUNT = 30.2
---
+++ variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "julia"
+idade = 30
+
+nome = "ana"
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+STATES = ["RJ", "SP", "MG"]
+
+STATES = 10
+
+STATES = "RIO", "BRASILIA"
