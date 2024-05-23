2891487a4d16c617203cde9ef923e648003ff0ff
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-05-23 00:05:39 -0300

    .

--- operadores_aritmeticos.py
+++ operadores_aritmeticos.py
@@ -10,3 +10,44 @@ produto_1 = 10
 produto_2 = 20
 
 print(produto_1 + produto_2 + 20)
+
+# Operadores de comparação
+# igualdade ==
+# diferença !=
+# > e >=
+# < e <=
+
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+
+# Operadores de atribuição
+saldo = 500
+saldo += 200
+saldo -= 200
+saldo *= 200
+saldo /= 200
+saldo //= 200
+saldo %= 200
+saldo **= 200
+
+limite = 100
+# Operadores logicos
+saldo >= saque and saque <= limite
+   
+saldo >= saque or saque <= limite
+
+contatos_emergencia = []
+not 1000 > 1500 #true
+
+not contatos_emergencia #true
+#lista vazia em python é falso
+
+not "saque 1500" #false
+
+not "" #true
+
