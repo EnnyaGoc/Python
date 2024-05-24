f0a06d283f93e04f6b91ce1a8706538ef90338a5
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-05-24 19:48:50 -0300

    .

--- identaçao.py
+++ identaçao.py
@@ -51,3 +51,20 @@ for letra in texto: #percorrer letra a letra no texto
 
 print()
 
+    # os 2 sao a mesma coisa
+
+for letra in texto: #percorrer letra a letra no texto
+    if letra.upper() in vogais:
+        print(letra, end="")
+else:
+    print() #adiciona uma quebra de linha
+
+
+#Funçao range - funçao built-in do py e é usada cm o for p produzir uma sequencia a partir de um inicio inclusivo e um fim exclusivo
+
+#range(stop) -> range object
+#range(start, stop[, step]) -> range object
+
+list(range(4)) #convertendo range p lista p exibir
+# [0, 1, 2, 3]
+
