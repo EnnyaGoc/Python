eaa86dd5c048cc135d5d4381e5c6ad3e5edce302
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-05-29 10:00:33 -0300

    Update listas.py

--- listas.py
+++ listas.py
@@ -98,38 +98,136 @@ for indice, carro in enumerate(carros):  #indice é o contador e o carro é o it
 
 #[].append - para adicionar elemento na lista
 
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista) # [1, "Python", [40, 30, 20]]
+
+
 
 
 #[].clear - para limpar a lista
 
+lista = [1, "Python", [40, 30, 20]]
+
+print(lista) # [1, "Python", [40, 30, 20]]
+
+lista.clear()
+
+print(lista) # []
+
+
+
 
 #[].copy - para retornar uma lista igual com outra instancia
+
+lista = [1, "Python", [40, 30, 20]]
+
+l2 = lista.copy()
+
+print(lista) # [1, "Python", [40, 30, 20]]
+
+print(l2) # [1, "Python", [40, 30, 20]]
+
+
 
 
 #[].count - para contar qntas vezes aparece o negocio na lista
 
+cores = ["vermelho", "azul", "verde", "azul"]
+
+cores.count("vermelho") # 1
+cores.count("azul") # 2
+cores.count("verde") # 1
+
+
+
 
 #[].extend - para adicionar elementos (uma lista adicionada a outra)
 
+linguagens = ["python", "js", "c"]
+
+print(linguagens) # ["python", "js", "c"]
+
+linguagens.extend(["java", "csharp"])
+
+print(linguagens) # ["python", "js", "c", "java", "csharp"]
+
+
+
 
 #[].index - para saber a primeira posiçao do objeto
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.index("java") # 3
+linguagens.index("python") # 0
+
 
 
 
 #[].pop - para adicionar itens da esq pra direita
 
+linguagens = ["python", "js", "c", "java", "csharp"]
 
+linguagens.pop() # csharp
+linguagens.pop() # java
+linguagens.pop() # c
+linguagens.pop(0) # python
 
 #[].remove - para remover itens pelo valor do item
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.remove("c")
+
+print(linguagens) # ["python", "js", "java", "csharp"]
+
+
+#[].reverse - reverter
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.reverse()
+
+print(linguagens) # ["csharp", "java", "c", "js", "python"]
+
 
 
 
 #[].sort - para ordenar a lista 
 
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort() # ["c", "csharp", "java", "js", "python"]
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(reverse=True) # ["python", "js", "java", "csharp", "c"]
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x)) # ["c", "js", "java", "python", "csharp"]
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x), reverse=True) # ["python", "csharp", "java", "js", "c"]
+
+
 
 
 #[].len - para mostrar o tamanho da lista
 
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+len(linguagens) # 5
+
+
 
 
 #[].sorted - para ordenar
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+sorted(linguagens, key=lambda x: len(x)) # ["c", "js", "java", "python", "csharp"]
+
+sorted(linguagens, key=lambda x: len(x), reverse=True) # ["python", "csharp", "java", "js", "c"]
