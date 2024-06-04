f7af30e2d70939a56d56eadf7b42ab0dc669dff8
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-06-04 00:22:02 -0300

    Create funcoes.py

---
+++ funcoes.py
@@ -0,0 +1,26 @@
+# def palavra reservada para funçao
+
+
+
+
+
+
+
+
+
+# em py, a funçao pode retornar mais de um valor
+
+
+
+
+
+
+# argumentos noemados - q vai passar chave, valor
+
+
+
+
+
+
+# args e kwargs -> *args e **kwargs
+# args os valores virao numa tupla, e kwargs num dicionario
