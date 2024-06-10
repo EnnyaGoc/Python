4fc26910a1591f14e76eccda09f8b1d2b1032b3b
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-06-10 15:27:48 -0300

    Create heranca.py

---
+++ poo/heranca.py
@@ -0,0 +1,20 @@
+
+#Herança simples - qndo a filha herda apenas uma classe pai 
+class A:
+    pass
+
+class B(A):   # b é filha da classe a
+   pass
+
+
+
+#Herança multipla - qndo a filha herda varias classes pai
+
+class A:
+    pass
+
+class B:  
+   pass
+
+class C(A, B):    
+    pass
