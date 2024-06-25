5ffd8e6baef04f0634a4a01a4e9943648db8c54b
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-06-25 13:15:33 -0300

    Update formatandodatahora.py

--- datahora/formatandodatahora.py
+++ datahora/formatandodatahora.py
@@ -21,9 +21,17 @@ print(type(data_convertida))
 
 #com pytz
 
+#é necessario instalar o pytz antes no terminal
+#pip install pyrz
 
+from datetime import datetime
+import pytz
 
+data = datetime.now(pytz.timezone("Europe/Oslo"))
+data2 = datetime.now(pytz.timezone("America/Sao_Paulo"))
 
+print(data)
+print(data2)
 
 
 #sem pytz
