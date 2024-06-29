b66e91499e218265798db12642993903b45d8437
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-06-29 02:59:08 -0300

    .

---
+++ pacotes/boaspraticas.py
@@ -0,0 +1,33 @@
+# https://peps.python.org/pep-0008/
+
+# 4 espaços p identaçao
+# 79 caracteres no maximo por linha
+# nome snake_case p funçao e variavel e CamelCase p classes 
+
+
+#flake8 p ajudar
+
+#pip install flake8
+#flake8 meu_script.py
+
+
+
+
+#ferramente q format automaticamente
+#Black
+
+#pip install black
+#black meu_script.py
+
+
+
+#isort
+
+import sys
+import os
+
+
+
+
+
+frutas = ['uva']
---
+++ pacotes/pip.py
@@ -0,0 +1,48 @@
+#Pip o um gerenciador de pacodes do python, q permite instalar, atualizar e remover pacotes facilmente
+
+#pip install numpy
+#pip uninstall numpy
+#pip list - mostra todos os pacotes instalados
+
+#------------------------
+#ambiente virtual
+
+#python3 -m venv nomedoproj
+#source nomedoproj/bin/activate    para ativar
+
+#deactivate para desativar
+
+#------------------------------
+#principais comandos do pip
+
+
+#pip install numpy
+
+#pip uninstall numpy
+
+#pip list
+
+#pip list --upgrade nomePacote - para atualizar
+
+#pip search termo_de_busca
+
+
+#--------------------------------------------------------
+#pipenv - ferramenta para gerenciar pacotes q combina gestao de dependencias cm criaçao de amb virtual
+
+#pip install pipenv
+#pipenv install numpy
+#pipenv uninstall numpy
+#pipenv lock 
+#pipenv graph - lista todas as dependencias
+
+
+#-------------------------------------------------------------
+
+#poetry - ourta q permite declarar bibliotecas q seu proj depende e gerencia elas p vc
+
+#pip install poetry
+#poetry new myproject
+#cd myproject
+#poetry add numpy
+#poetry remove numpy
